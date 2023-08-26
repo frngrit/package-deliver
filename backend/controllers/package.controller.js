4a4182e74dea66b0d1f9fc5fa89995fa64cc6b2a
Test <test@PiMacs-MacBook-Air.local>
2023-08-26 23:17:06 +0700

    backend: change package api response

--- backend/controllers/package.controller.js
+++ backend/controllers/package.controller.js
@@ -1,7 +1,12 @@
 const query = require("../config/db")
 
 const getPackages = async (req, res) => {
-    const sql = `SELECT * FROM package_db.Packages;`
+    const sql = `
+    SELECT pg.Id, pg.TrackingNo, own.Id, own.Name, own.Room, own.PhoneNumber, pg.isRecieved, pg.CreatedAt
+    FROM package_db.Packages as pg 
+    JOIN package_db.Owners as own 
+    ON pg.OwnerId = own.Id;
+    `
 
     const result = await query(sql)
 
