84a247390134c0df79f8531b88fd3e48ba5a0098
frngrit <frank.12876@hotmail.com>
2023-08-27 12:12:12 +0700

    backend: fix test case

--- backend/tests/services/package.service.test.js
+++ backend/tests/services/package.service.test.js
@@ -35,29 +35,6 @@ const packageRepository = () => {
 
 describe('packageService: getPackages', () => {
   test('should return list of packages', () => {
-
-    //Arrange
-    const packages = [
-      {
-        "Id": 1,
-        "TrackingNo": "TH-129248O2",
-        "OwnerId": "Frank",
-        "RoomId": "5/191",
-        "PhoneNumber": "084-535-4871",
-        "isRecieved": 0,
-        "CreatedAt": "2023-08-26T07:39:09.000Z"
-      },
-      {
-        "Id": 2,
-        "TrackingNo": "TH-15393598",
-        "OwnerId": "Cream",
-        "RoomId": "5/191",
-        "PhoneNumber": "084-535-4871",
-        "isRecieved": 0,
-        "CreatedAt": "2023-08-26T07:39:09.000Z"
-      }
-    ]
-
     const mockRepo = packageRepository()
 
     //Act
