8d701d1f717208498700110170d9567fedc39085
rerngrit <frank.12876@hotmail.com>
2024-04-20 02:19:08 +0700

    feat: change loggin format

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8081;
-var cors = require('cors')
+var cors = require("cors");
 require("dotenv").config();
 
-var morgan = require('morgan')
-morgan('tiny')
-morgan(':method :url :status :res[content-length] - :response-time ms')
+var morgan = require("morgan");
 
-app.use(cors())
-app.use(express.json())
-app.use(morgan('combined'))
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
-app.get('/test', (req, res) => res.json({'message': 'hello'}));
+app.get("/test", (req, res) => res.json({ message: "hello" }));
 
-app.use('/api/packages', require("./routes/package.route"))
-app.use('/api/owners', require("./routes/owner.route"))
+app.use("/api/packages", require("./routes/package.route"));
+app.use("/api/owners", require("./routes/owner.route"));
 
-app.use('/api/health', require("./routes/health.route"))
+app.use("/api/health", require("./routes/health.route"));
 
-app.listen(PORT, () => console.log(`app running on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`app running on http://localhost:${PORT}`));
